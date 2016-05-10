698221e47612f0a2cb350522d708b41a5f87e733
dance2die <dontbothermewithspam@gmail.com>
2016-05-10 07:10:22 -0400

    adding a class within a template

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -3,9 +3,15 @@ import {Component} from 'angular2/core'
 @Component({
     selector: 'my-component',
     template: `
-        This is my component!
-    `
+        Hi I'm {{name}}. This is my first angular 2 app.
+        <span [class.is-awesome]="inputElement.value === 'yes'">It's awesome</span>
+        <br />
+        <br />
+        This line is great.
+        <input type="text" #inputElement (keyup)="0">
+    `,
+    styleUrls: ['src/css/mycomponent.css']
 })
 export class MyComponentComponent{
-
+    name = "Eddie";
 }
