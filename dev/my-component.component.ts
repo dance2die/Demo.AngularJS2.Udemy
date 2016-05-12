02d3f459324c88fbe1a5a6db44555fb9a31ddbff
dance2die <dontbothermewithspam@gmail.com>
2016-05-12 06:48:27 -0400

    added directives to add colors and enable/disable a button.

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -3,12 +3,15 @@ import {Component} from 'angular2/core'
 @Component({
     selector: 'my-component',
     template: `
-        Hi I'm {{name}}. This is my first angular 2 app.
+        Hi I'm <span [style.color]="inputElement.value === 'yes' ? 'red' : ''">{{name}}</span>. This is my first angular 2 app.
         <span [class.is-awesome]="inputElement.value === 'yes'">It's awesome</span>
         <br />
         <br />
         This line is great.
         <input type="text" #inputElement (keyup)="0">
+        <br />
+        <br />
+        <button [disabled]="inputElement.value !=='yes'">Enabled only "yes" was entered</button>
     `,
     styleUrls: ['src/css/mycomponent.css']
 })
